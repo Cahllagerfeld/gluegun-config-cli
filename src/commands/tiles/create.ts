64bc68fd94530c71412ac3b7d8402b5d2e03629d
Cahllagerfeld <Cahllagerfeld>
2021-12-25 20:21:55 +0000

    feat: update init command

--- src/commands/tiles/create.ts
+++ src/commands/tiles/create.ts
@@ -14,17 +14,33 @@ export default {
         {
           title: 'Get current Config',
           task: async (ctx, task) => {
-            ctx.currentConfig = await fs.promises.readFile('config.json')
+            const parsedString = await fs.promises.readFile(
+              'config.json',
+              'utf8'
+            )
+            ctx.currentConfig = JSON.parse(parsedString)
           }
         },
         {
           title: 'Get Object Name',
           task: async (ctx, task) => {
-            console.log(ctx.currentConfig)
             ctx.input = await task.prompt({
               type: 'Input',
               message: 'Whats the name of the object?'
             })
+          }
+        },
+        {
+          title: 'Create new Tile',
+          task: async (ctx, task) => {
+            ctx.currentConfig.tiles = [
+              ...ctx.currentConfig.tiles,
+              ...[{ name: ctx.input }]
+            ]
+            await fs.promises.writeFile(
+              'config.json',
+              JSON.stringify(ctx.currentConfig)
+            )
           }
         }
       ],
